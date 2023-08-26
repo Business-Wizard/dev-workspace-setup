9a2390651852990ed8a42c1eb2831a71cc6b7a47
Business-Data-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2023-08-26 13:26:25 -0600

    chore: apply formatters

--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 import pathlib
+
+import yaml
 from invoke import tasks
 from invoke.context import Context
-import yaml
 
 SECRETS = pathlib.Path('secrets.yaml')
 
@@ -11,15 +12,15 @@ SECRETS = pathlib.Path('secrets.yaml')
 def update_vscode(context: Context):
     password: str = _read_secrets()['password']
 
-    cmd_clone_repo = "git clone https://aur.archlinux.org/visual-studio-code-insiders-bin.git"
+    cmd_clone_repo = (
+        "git clone https://aur.archlinux.org/visual-studio-code-insiders-bin.git"
+    )
     context.run(command=cmd_clone_repo)
     repo_path = pathlib.Path("visual-studio-code-insiders-bin")
     with context.cd(path=repo_path):
         context.run(command="echo 'Y' | makepkg -si")
         context.sudo(command="pacman -U", password=password)
-    context.sudo(
-        command="rm -rf ./visual-studio-code-insiders-bin", password=password
-    )
+    context.sudo(command="rm -rf ./visual-studio-code-insiders-bin", password=password)
 
 
 def _read_secrets() -> dict[str, str]:
