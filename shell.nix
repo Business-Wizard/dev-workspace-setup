7906ecd1d87361f57ff8d4b962e56ca75addda34
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2024-07-17 11:47:30 -0600

    build: add initial nix shell
    Signed-off-by: Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+let
+  shell_packages = with pkgs; [
+    pre-commit
+    rye
+  ];
+in
+
+pkgs.mkShell {
+  buildInputs = shell_packages;
+
+  shellHook = ''
+    if [ ! -d ".venv" ]; then
+      echo "Creating a virtual environment..."
+      rye venv create .venv
+    fi
+
+    # Activate the virtual environment
+    echo "Activating the virtual environment..."
+    source .venv/bin/activate
+  '';
+}
