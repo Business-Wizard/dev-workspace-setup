18bbb3b53b2fec2faeff419cc7c16d6d3907418e
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2024-07-17 13:19:22 -0600

    chore: rm unused vscode update task
    Signed-off-by: Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>

--- tasks.py
+++ tasks.py
@@ -1,27 +1,3 @@
-import pathlib
+from pathlib import Path
 
-import yaml
-from invoke import tasks
-from invoke.context import Context
-
-SECRETS = pathlib.Path("secrets.yaml")
-
-
-# create task to update vscode
-@tasks.task  # pyright: ignore[reportUnknownMemberType]
-def update_vscode(context: Context):
-    password: str = _read_secrets()["password"]
-
-    cmd_clone_repo = (
-        "git clone https://aur.archlinux.org/visual-studio-code-insiders-bin.git"
-    )
-    context.run(command=cmd_clone_repo)
-    repo_path = pathlib.Path("visual-studio-code-insiders-bin")
-    with context.cd(path=repo_path): # pyright: ignore[reportUnknownMemberType]
-        context.run(command="echo 'Y' | makepkg -si")
-        context.sudo(command="pacman -U", password=password)
-    context.sudo(command="rm -rf ./visual-studio-code-insiders-bin", password=password)
-
-
-def _read_secrets() -> dict[str, str]:
-    return yaml.load(stream=SECRETS.read_text(), Loader=yaml.SafeLoader)
+SECRETS = Path("secrets.yaml")
